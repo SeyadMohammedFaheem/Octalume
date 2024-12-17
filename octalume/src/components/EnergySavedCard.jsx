7ace3d6a936376e00bcd81a01871fc64ec11f611
SeyadMohammedFaheem <faheemseyadmd@gmail.com>
2024-12-18 00:54:43 +0530

    test

--- octalume/src/components/EnergySavedCard.jsx
+++ octalume/src/components/EnergySavedCard.jsx
@@ -6,7 +6,7 @@ const EnergySavedCard = () => {
       {/* Content Section */}
       <div className="flex flex-col gap-2">
       <h2 className="font-primary text-primary-h2 text-primary-text-inverse gap-2">Electricity Consumed By Rooms</h2>
-      <p className="text-secondary-text">Peephole Cam (Video Doorbell)</p>
+      <p className="text-secondary-text">Peephole Camera (Video Doorbell)</p>
 
         <div className="flex items-end gap-2 mt-2 justify-between w-full ">
           <span className="text-4xl w-2/4 font-bold">4300 kWh</span>
